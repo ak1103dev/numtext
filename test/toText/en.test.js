ecd3864570084e6d1c0fb0bca05a3dfb385987d6
ak1103dev <ak.apichan1103@gmail.com>
2017-07-24 10:07:04 +0700

    :construction: the hundreds and the thousands

--- test/toText/en.test.js
+++ test/toText/en.test.js
@@ -34,4 +34,40 @@ describe('toEngText', () => {
       });
     });
   });
+  describe('the hundreds', () => {
+    const text = [
+      'One Hundred', 'Two Hundred', 'Three Hundred', 'Four Hundred', 'Five Hundred',
+      'Six Hundred', 'Seven Hundred', 'Eight Hundred', 'Nine Hundred',
+      'One Hundred Twenty Three', 'Nine Hundred Nine', 'Seven Hundred Sixty',
+    ];
+    const num = [
+      100, 200, 300, 400, 500, 600, 700, 800, 900,
+      123, 909, 760,
+    ];
+    _.map(text, (t, index) => {
+      it(`should return "${t}" when number is ${num[index]}`, () => {
+        const input = num[index];
+        const expectedValue = t;
+        expect(toText(input, { lang: 'en' })).toEqual(expectedValue);
+      });
+    });
+  });
+  describe('the thousands', () => {
+    const text = [
+      'One Thousand', 'Two Thousand', 'Three Thousand', 'Four Thousand', 'Five Thousand',
+      'Six Thousand', 'Seven Thousand', 'Eight Thousand', 'Nine Thousand',
+      'One Thousand One Hundred Ten', 'One Thousand One Hundred Eleven',
+    ];
+    const num = [
+      1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000,
+      1110, 1111,
+    ];
+    _.map(text, (t, index) => {
+      it(`should return "${t}" when number is ${num[index]}`, () => {
+        const input = num[index];
+        const expectedValue = t;
+        expect(toText(input, { lang: 'en' })).toEqual(expectedValue);
+      });
+    });
+  });
 });
